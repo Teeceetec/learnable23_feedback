bf24d9cfd6ed30da6f8430a097321d55e75d839b
onyia justin <onyiajustin@gmail.com>
2024-03-19 09:13:08 +0100

    New commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 
-
+ 
 const contractAddress = '0xEDf6c43ABc3c16B39A4f89e5cF46E5E6FE06c6F1'; 
 const abi = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"string","name":"feedback","type":"string"}],"name":"FeedbackAdded","type":"event"},{"inputs":[{"internalType":"string","name":"_feedback","type":"string"}],"name":"addFeedback","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"feedbacks","outputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"string","name":"feedback","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"getFeedback","outputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getFeedbackCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]; 
 
